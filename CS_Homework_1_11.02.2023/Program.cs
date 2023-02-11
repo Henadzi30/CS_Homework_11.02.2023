9f9a9b36aeb47b6c1d22c32ec4ac79816b0f0ae0
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-11 17:16:54 +0300

    an algorithm for determining the maximum and minimum numbers has been created

---
+++ CS_Homework_1_11.02.2023/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число:");
+string first_number = Console.ReadLine();
+int user_first_number = Convert.ToInt32(first_number);
+Console.WriteLine("Введите второе число:");
+string second_number = Console.ReadLine();
+int user_second_number = Convert.ToInt32(second_number);
+int max_num = 0;
+int min_num = 0;
+if (user_first_number > user_second_number)
+{
+    max_num = user_first_number;
+    min_num = user_second_number;
+}
+if (user_first_number < user_second_number)
+{
+    max_num = user_second_number;
+    min_num = user_first_number;
+}
+Console.Write("max = ");
+Console.WriteLine(max_num);
+Console.Write("min = ");
+Console.WriteLine(min_num);
