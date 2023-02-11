eff7b9aceecd504ed8a652c2b6e28a4f0f3ab6f1
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-11 18:11:53 +0300

    an algorithm for determining the maximum and minimum of three numbers has been created

---
+++ CS_Homework_2_11.02.2023/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число а");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max_num = 0;
+
+if (numberA > numberB & numberA > numberC)
+{
+    max_num = numberA;
+}
+else if (numberB > numberA & numberB > numberC)
+{
+    max_num = numberB;
+}
+else if (numberC > numberA & numberC > numberB)
+{
+    max_num = numberC;
+}
+Console.WriteLine(max_num);
