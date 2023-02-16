fa8d212d953b5307c4b302523e666cd06a6639c2
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-16 20:24:05 +0300

    added a method to print the therd digit of number

---
+++ CS_Homework_13_14.02.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+if (numberOne >= 100)
+{
+    while (numberOne > 999)
+    {
+        numberOne = numberOne / 10;
+    }
+    Console.WriteLine(numberOne % 10);  
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
