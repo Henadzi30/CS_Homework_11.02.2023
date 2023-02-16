188b49a6cae62e3d67d6cb705fa5bd676d54803e
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-16 21:14:08 +0300

    added a method for determining whether a digit indicating the day of the week is a holiday

---
+++ CS_Homework_15_14.02.2023/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+
+Console.WriteLine("Введите число, обозначающее день недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+if (1 <= dayNum & dayNum <= 5)
+{
+    Console.WriteLine("нет");
+}
+if (dayNum == 6 | dayNum == 7)
+{
+    Console.WriteLine("да");
+}
