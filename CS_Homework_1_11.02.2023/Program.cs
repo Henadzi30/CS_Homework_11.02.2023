4bcb080547b52b053293c1f8d7d13bb0bdd69428
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-16 18:41:25 +0300

    modified Homework_1

--- CS_Homework_1_11.02.2023/Program.cs
+++ CS_Homework_1_11.02.2023/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите первое число:");
-string first_number = Console.ReadLine();
+string? first_number = Console.ReadLine();
 int user_first_number = Convert.ToInt32(first_number);
 Console.WriteLine("Введите второе число:");
-string second_number = Console.ReadLine();
+string? second_number = Console.ReadLine();
 int user_second_number = Convert.ToInt32(second_number);
 int max_num = 0;
 int min_num = 0;
