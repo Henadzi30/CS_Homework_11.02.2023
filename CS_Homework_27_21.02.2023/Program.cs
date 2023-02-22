e448b1474d42b4ef8564c3f5ac8c7cb2b933f8d8
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-22 20:05:39 +0300

    added method of summing digits of a given number

---
+++ CS_Homework_27_21.02.2023/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Задаем метод считывания числа, вводимого пользователем 
+int ReadNumber(string messegeToUser)
+{
+    Console.WriteLine(messegeToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+// Определяем метод суммирования цифр числа, заданного пользователем
+int sumDigitOfNumber(int num)
+{
+    int sumDigit = 0;
+    for(int i = 0; i < num; i++)
+    {
+        sumDigit += num % 10;
+        num /= 10;
+    }
+    return sumDigit;
+}
+
+// Блок запроса информации от пользователя (начало программы) и вывода результата
+int number = ReadNumber("Введите число: ");
+int result = sumDigitOfNumber(number);
+Console.WriteLine(result);
