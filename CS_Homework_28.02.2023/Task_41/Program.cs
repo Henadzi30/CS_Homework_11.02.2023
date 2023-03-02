3e5af2f97f2859bb228fefcc7dd84dd0227edd55
Henadzi_Misuna <henadzi30@gmail.com>
2023-03-02 21:27:16 +0300

    added a method for finding the coordinates of the point of intersection of lines

---
+++ CS_Homework_28.02.2023/Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+// Задаем метод создания массива чисел от пользователя
+int[] ReadNumber(string messegeToUser)
+{
+    Console.Write(messegeToUser);
+    int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return array;
+}
+
+// Задаем метод подсчета чисел больше нуля 
+int NumbersGreaterZero(int[] UserArray)
+{
+    int count = 0;
+    for (int i = 0; i < UserArray.Length; i++)
+    {
+        if (UserArray[i] > 0)
+        {
+            count++;
+        }      
+    }
+    return count;
+}
+
+// Запрос информации от пользователя
+int[] listOfNumbers = ReadNumber("Введите числа через пробел: ");
+
+// Блок формирования и вывода информации в терминал
+int CountNumbersGreaterZero = NumbersGreaterZero(listOfNumbers);
+Console.WriteLine($"Количество чисел в массиве, больших нуля: {string.Join(", ", CountNumbersGreaterZero)}");
---
+++ CS_Homework_28.02.2023/Task_43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//  Метод создания переменных на основании данных пользователя
+
+double ReadNumber(string messegeToUser)
+{
+    Console.Write(messegeToUser);
+    double value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+// Метод нахождения координат точки пересечения прямых
+(double, double) PointIntersectionLines(double numberK1, double numberB1, double numberK2, double numberB2)
+{
+    double pointCoordinateX = (-numberB2 + numberB1) / (-numberK1 + numberK2);
+    double pointCoordinateY = numberK2 * (-numberB2 + numberB1) / (-numberK1 + numberK2) + numberB2;  
+    return (pointCoordinateX, pointCoordinateY);
+}
+
+// Блок запроса данных от пользователя
+double coefficientK1 = ReadNumber("Введите значение k1: ");
+double coefficientB1 = ReadNumber("Введите значение b1: ");
+double coefficientK2 = ReadNumber("Введите значение k2: ");
+double coefficientB2 = ReadNumber("Введите значение b2: ");
+
+// Блок проверки условий
+if (coefficientK1 == coefficientK2 && coefficientB1 == coefficientB2)
+{
+    Console.Write($"По заданным данным прямые совпадают.");
+}
+else if (coefficientK1 == coefficientK2 && coefficientB1 != coefficientB2)
+{
+    Console.Write($"По заданным данным прямые параллельны.");
+}
+
+// Вывод результатов в терминал
+Console.Write("По заданным данным прямые пересекаются в точке с координатами (x, y): ");
+Console.WriteLine($"{PointIntersectionLines(coefficientK1, coefficientB1, coefficientK2, coefficientB2)}");
