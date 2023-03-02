3b4baba6935fd5ee93d0a32d39ab13aae24c101e
Henadzi_Misuna <henadzi30@gmail.com>
2023-03-02 21:39:07 +0300

    modified a method for counting the number of numbers greater then zero in an array

--- CS_Homework_28.02.2023/Task_41/Program.cs
+++ CS_Homework_28.02.2023/Task_41/Program.cs
@@ -4,7 +4,6 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-
 // Задаем метод создания массива чисел от пользователя
 int[] ReadNumber(string messegeToUser)
 {
@@ -19,17 +18,13 @@ int NumbersGreaterZero(int[] UserArray)
     int count = 0;
     for (int i = 0; i < UserArray.Length; i++)
     {
-        if (UserArray[i] > 0)
-        {
-            count++;
-        }      
+        if (UserArray[i] > 0) count++;      
     }
     return count;
 }
 
 // Запрос информации от пользователя
 int[] listOfNumbers = ReadNumber("Введите числа через пробел: ");
-
 
 // Блок формирования и вывода информации в терминал
 int CountNumbersGreaterZero = NumbersGreaterZero(listOfNumbers);
