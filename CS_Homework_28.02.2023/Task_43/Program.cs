bb60568e2feb47442f63b37b85b13e0168c21773
Henadzi_Misuna <henadzi30@gmail.com>
2023-03-03 15:23:08 +0300

    added a reference examples for solving problems 41 & 43

--- CS_Homework_28.02.2023/Task_43/Program.cs
+++ CS_Homework_28.02.2023/Task_43/Program.cs
@@ -6,40 +6,40 @@
 
 //  Метод создания переменных на основании данных пользователя
 
-// double ReadNumber(string messegeToUser)
-// {
-//     Console.Write(messegeToUser);
-//     double value = Convert.ToInt32(Console.ReadLine());
-//     return value;
-// }
+double ReadNumber(string messegeToUser)
+{
+    Console.Write(messegeToUser);
+    double value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
 
-// // Метод нахождения координат точки пересечения прямых
-// (double, double) PointIntersectionLines(double numberK1, double numberB1, double numberK2, double numberB2)
-// {
-//     double pointCoordinateX = (-numberB2 + numberB1) / (-numberK1 + numberK2);
-//     double pointCoordinateY = numberK2 * (-numberB2 + numberB1) / (-numberK1 + numberK2) + numberB2;
-//     return (pointCoordinateX, pointCoordinateY);
-// }
+// Метод нахождения координат точки пересечения прямых
+(double, double) PointIntersectionLines(double numberK1, double numberB1, double numberK2, double numberB2)
+{
+    double pointCoordinateX = (-numberB2 + numberB1) / (-numberK1 + numberK2);
+    double pointCoordinateY = numberK2 * (-numberB2 + numberB1) / (-numberK1 + numberK2) + numberB2;
+    return (pointCoordinateX, pointCoordinateY);
+}
 
-// // Блок запроса данных от пользователя
-// double coefficientK1 = ReadNumber("Введите значение k1: ");
-// double coefficientB1 = ReadNumber("Введите значение b1: ");
-// double coefficientK2 = ReadNumber("Введите значение k2: ");
-// double coefficientB2 = ReadNumber("Введите значение b2: ");
+// Блок запроса данных от пользователя
+double coefficientK1 = ReadNumber("Введите значение k1: ");
+double coefficientB1 = ReadNumber("Введите значение b1: ");
+double coefficientK2 = ReadNumber("Введите значение k2: ");
+double coefficientB2 = ReadNumber("Введите значение b2: ");
 
-// // Блок проверки условий
-// if (coefficientK1 == coefficientK2 && coefficientB1 == coefficientB2)
-// {
-//     Console.Write($"По заданным данным прямые совпадают.");
-// }
-// else if (coefficientK1 == coefficientK2 && coefficientB1 != coefficientB2)
-// {
-//     Console.Write($"По заданным данным прямые параллельны.");
-// }
+// Блок проверки условий
+if (coefficientK1 == coefficientK2 && coefficientB1 == coefficientB2)
+{
+    Console.Write($"По заданным данным прямые совпадают.");
+}
+else if (coefficientK1 == coefficientK2 && coefficientB1 != coefficientB2)
+{
+    Console.Write($"По заданным данным прямые параллельны.");
+}
 
-// // Вывод результатов в терминал
-// Console.Write("По заданным данным прямые пересекаются в точке с координатами (x, y): ");
-// Console.WriteLine($"{PointIntersectionLines(coefficientK1, coefficientB1, coefficientK2, coefficientB2)}");
+// Вывод результатов в терминал
+Console.Write("По заданным данным прямые пересекаются в точке с координатами (x, y): ");
+Console.WriteLine($"{PointIntersectionLines(coefficientK1, coefficientB1, coefficientK2, coefficientB2)}");
 
 const int COEFFICIENT = 0;
 const int CONSTANT = 1;
